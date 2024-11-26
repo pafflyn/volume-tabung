f766d6da4aff9be6b1a48282c093a622e6d24de5
pafflyn <rikodwianggoro42@gmail.com>
2024-11-26 16:25:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+import math, time
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Menghitung volume tabung")
+
+r = number = st.number_input('Masukkan Jari-Jari (cm):', 0)
+t = number = st.number_input('Masukkan Diameter(cm):', 0)
+
+if st.button("Hitung volume", type='primary'):
+    loading = st.progress(0)
+    for i in range(100):
+        time.sleep(0.01)
+        loading.progress(i+1)
+   
+   
+    v = math.pi*(r**2)*t
+    st.success(f'Volume tabung adalah {v:2f}')
